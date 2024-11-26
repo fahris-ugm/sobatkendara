b36fd3eaab5d74197a2f21e26c14c7f5f00ebdc9
Fahris <fahrismumtazaahsan@mail.ugm.ac.id>
2024-11-26 09:59:38 +0700

    implement about screen

--- app/src/main/java/id/ac/ugm/fahris/sobatkendara/LoginActivity.kt
+++ app/src/main/java/id/ac/ugm/fahris/sobatkendara/LoginActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -27,7 +28,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -118,6 +121,14 @@ fun LoginScreen(
             .padding(16.dp),
         verticalArrangement = Arrangement.Center
     ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.sobatkendara_logo),
+            contentDescription = "Logo",
+            modifier = Modifier.padding(bottom = 16.dp, start = 32.dp, end = 32.dp),
+            contentScale = ContentScale.FillWidth
+        )
+        Spacer(modifier = Modifier.height(32.dp))
 
         OutlinedTextField(
             value = email,
--- app/src/main/java/id/ac/ugm/fahris/sobatkendara/ui/AboutScreen.kt
+++ app/src/main/java/id/ac/ugm/fahris/sobatkendara/ui/AboutScreen.kt
@@ -10,6 +10,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+import id.ac.ugm.fahris.sobatkendara.R
 import id.ac.ugm.fahris.sobatkendara.ui.components.AppBar
 
 
@@ -21,11 +37,66 @@ fun AboutScreen(
         topBar = { AppBar(drawerState = drawerState) }
     ) {
         Column(
-            modifier = Modifier.fillMaxSize().padding(it),
+            modifier = Modifier.fillMaxSize().padding(it).padding(16.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("About")
+            // Logo
+            Image(
+                painter = painterResource(id = R.drawable.sobatkendara_logo),
+                contentDescription = "Logo",
+                modifier = Modifier.padding(bottom = 16.dp, start = 32.dp, end = 32.dp),
+                contentScale = ContentScale.FillWidth
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Text(
+                text = stringResource(R.string.app_fullname),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(bottom = 8.dp).align(Alignment.CenterHorizontally)
+            )
+
+            Text(
+                text = stringResource(R.string.app_nim),
+                fontSize = 18.sp,
+                color = Color.Gray,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(bottom = 8.dp).align(Alignment.CenterHorizontally)
+            )
+
+            // Description
+            Text(
+                text = stringResource(R.string.app_description),
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(bottom = 8.dp).align(Alignment.CenterHorizontally)
+            )
+
+            // Institution
+            Text(
+                text = stringResource(R.string.app_institution),
+                fontSize = 18.sp,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.padding(bottom = 16.dp).align(Alignment.CenterHorizontally)
+            )
+
+            // Year
+            Text(
+                text = stringResource(R.string.app_year),
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center,
+                color = Color.Gray
+            )
         }
     }
+}
+
+@Preview
+@Composable
+fun AboutScreenPreview() {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    AboutScreen(drawerState)
 }
